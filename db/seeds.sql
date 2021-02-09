36e77b6c8efc080fcb7bdeae23976d6ebab3d5cd
tafseer-khan <tafseer.k.khan@gmail.com>
2021-02-08 18:47:25 -0600

    Created file and directory structure

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");   
+INSERT INTO burgers (burger_name) VALUES ("Impossible Burger");   
+INSERT INTO burgers (burger_name) VALUES ("Black Bean Burger");   
+INSERT INTO burgers (burger_name) VALUES ("Pattymelt");
+
+SELECT * FROM burgers;
