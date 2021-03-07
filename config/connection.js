d02070823aa7529637030a995eeea757ae7e565c
tafseer-khan <tafseer.k.khan@gmail.com>
2021-03-07 17:31:13 -0600

    updated connection for jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: 'AzAq69SxSw',
-  database: 'burgers_db',
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "AzAq69SxSw",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect((err) => {
